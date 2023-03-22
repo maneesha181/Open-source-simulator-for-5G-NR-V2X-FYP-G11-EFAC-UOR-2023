91f1e6a1d60bf133ec7f41d78c528252b2fecdb3
maneesha181 <maneesha.nethmini96@gmail.com>
2023-03-23 00:47:28 +0530

    add comments to the initial code

--- point_to_point_topology.cc
+++ point_to_point_topology.cc
@@ -1,8 +1,6 @@
-// open source simulator for 5G NR V2X communication is designed and simulated by G11 FOE-UOR 
-// as the undergraduate project 2023.
-// POINT-TO-POINT NETWORK TOPOLOGY
-// In this example it consists of two nodes which are named as UDP echo application client
-// and UDP echo application server. 
+// This is a C++ program for a point-to-point network topology example
+// It simulates a two-node network consisting of a UDP echo application client and a UDP echo application server
+// The program is designed and simulated by G11 FOE-UOR as an undergraduate project in 2023
 
 #include "ns3/core-module.h"
 #include "ns3/network-module.h"
@@ -10,40 +8,38 @@
 #include "ns3/point-to-point-module.h"
 #include "ns3/applications-module.h"
 
- 
 using namespace ns3;
 
-NS_LOG_COMPONENT_DEFINE ("PointToPointScriptExample"); // define log component
+NS_LOG_COMPONENT_DEFINE ("PointToPointScriptExample"); // define log component for debugging purposes
 
-int
-main (int argc, char *argv[])
+int main (int argc, char *argv[])
 {
-  CommandLine cmd (__FILE__);
-  cmd.Parse (argc, argv);
-  
-  Time::SetResolution (Time::NS);
-  LogComponentEnable ("UdpEchoClientApplication", LOG_LEVEL_INFO);
-  LogComponentEnable ("UdpEchoServerApplication", LOG_LEVEL_INFO);
+CommandLine cmd (FILE);
+cmd.Parse (argc, argv);
 
-  NodeContainer nodes;
-  nodes.Create (2);
+Time::SetResolution (Time::NS); // set time resolution
+LogComponentEnable ("UdpEchoClientApplication", LOG_LEVEL_INFO); // enable logging for client application
+LogComponentEnable ("UdpEchoServerApplication", LOG_LEVEL_INFO); // enable logging for server application
 
-  PointToPointHelper pointToPoint;
-  pointToPoint.SetDeviceAttribute ("DataRate", StringValue ("5Mbps"));
-  pointToPoint.SetChannelAttribute ("Delay", StringValue ("2ms"));
+NodeContainer nodes; // create node container for two nodes
+nodes.Create (2);
 
-  NetDeviceContainer devices;
-  devices = pointToPoint.Install (nodes);
+PointToPointHelper pointToPoint; // create point-to-point helper
+pointToPoint.SetDeviceAttribute ("DataRate", StringValue ("5Mbps")); // set data rate to 5 Mbps
+pointToPoint.SetChannelAttribute ("Delay", StringValue ("2ms")); // set delay to 2ms
 
-  InternetStackHelper stack;
-  stack.Install (nodes);
+NetDeviceContainer devices; // create network device container
+devices = pointToPoint.Install (nodes); // install devices on the nodes
 
-  Ipv4AddressHelper address;
-  address.SetBase ("10.1.1.0", "255.255.255.0");
+InternetStackHelper stack; // create internet stack helper
+stack.Install (nodes); // install stack on the nodes
 
-  Ipv4InterfaceContainer interfaces = address.Assign (devices);
+Ipv4AddressHelper address; // create IP address helper
+address.SetBase ("10.1.1.0", "255.255.255.0"); // set IP address and subnet mask
 
-  UdpEchoServerHelper echoServer (9);
+Ipv4InterfaceContainer interfaces = address.Assign (devices); // assign IP addresses to the devices
+
+UdpEchoServerHelper echoServer (9); // create server application on port
 
   ApplicationContainer serverApps = echoServer.Install (nodes.Get (1));
   serverApps.Start (Seconds (1.0));
